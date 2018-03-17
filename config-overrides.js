2c0d6e68139e7144565383a88c50f63c0c5664c5
Mark <mark@remarkablemark.org>
2018-03-17 13:39:41 -0400

    feat(config): replace `react` with `preact` in production build

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,19 @@
+/**
+ * Config overrides.
+ *
+ * https://github.com/timarney/react-app-rewired#extended-configuration-options
+ */
 module.exports = (config, env) => {
+  // replace `react` with `preact` in production
+  if (env.NODE_ENV === 'production') {
+    config.resolve = {
+      alias: {
+        react: 'preact-compat',
+        'react-dom': 'preact-compat',
+      },
+    };
+  }
+
+  // otherwise use default `create-react-app` config
   return config;
 };
