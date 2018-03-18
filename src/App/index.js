dd04923d4df81b189cc521d34d5f3905f60c2a85
Mark <mark@remarkablemark.org>
2018-03-18 19:13:58 -0400

    refactor(App): refactor App classNames to BEM syntax

--- src/App/index.js
+++ src/App/index.js
@@ -5,8 +5,8 @@ import './styles.css';
 export default class App extends Component {
   render() {
     return (
-      <main className="App-center" role="main">
-        <h1 className="App-heading text-center">Tile Slider</h1>
+      <main className="App__main" role="main">
+        <h1 className="App__h1 text-center">Tile Slider</h1>
         <TileSlider size={3} />
       </main>
     );
