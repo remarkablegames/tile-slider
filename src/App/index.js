61025a93d00c992bd28ea3681f25bc8d983ec73d
Mark <mark@remarkablemark.org>
2018-03-18 18:31:23 -0400

    feat(App): render <TileSlider> in <App>

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TileSlider from '../TileSlider';
 import './styles.css';
 
 export default class App extends Component {
@@ -6,6 +7,7 @@ export default class App extends Component {
     return (
       <main className="center" role="main">
         <h1 className="heading text-center">Tile Slider</h1>
+        <TileSlider size={3} />
       </main>
     );
   }
