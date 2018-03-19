55c06bd73ca831097c411b701f925173271c2579
Mark <mark@remarkablemark.org>
2018-03-18 20:04:06 -0400

    feat(index): use requirejs to load es5 polyfill before app in prod

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const bootstrap = () => {
+  const App = require('./App').default;
+  render(<App />, document.getElementById('root'));
+};
+
+if (process.env.NODE_ENV === 'development' || !window.requirejs) {
+  bootstrap();
+} else {
+  window.requirejs(
+    ['https://cdn.polyfill.io/v2/polyfill.min.js?features=es5&flags=gated'],
+    bootstrap,
+    // http://requirejs.org/docs/api.html#errbacks
+    bootstrap
+  );
+}
+
 registerServiceWorker();
